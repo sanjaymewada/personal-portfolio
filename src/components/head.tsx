5cb590b0ec5e5dd8131556ff978337565c407cde
Sanjay Mewada <sanjay.saregama@gmail.com>
2025-10-28 14:03:32 +0530

    Update author meta tag in head component

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -8,7 +8,7 @@ export function Head({ metadata }: { metadata: Metadata }) {
 
       <meta charSet='utf-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta name='author' content='HARSHHAA' />
+      <meta name='author' content='Sanjay Mewada' />
       <meta name='robots' content='index, follow' />
 
       <meta property='og:title' content={metadata.title?.toString()} />
